3be6b2ad72237b76cb1b6a846a545a77b984bb48
yo666ha-boop <yo666ha@gmail.com>
2026-08-17 10:55:17 +0900

    Fix Rin portrait path for v2.15.28

--- shogi-side-test/final21513_rinimg.js
+++ shogi-side-test/final21513_rinimg.js
@@ -1,1 +1,1 @@
-window.FINAL21513_IMAGES=window.FINAL21513_IMAGES||{};window.FINAL21513_IMAGES.rin='./rin21515.jpg?v=21515';
+window.FINAL21513_IMAGES=window.FINAL21513_IMAGES||{};window.FINAL21513_IMAGES.rin=new URL('../shogi-side-test/rin21515.jpg?v=21515',location.href).href;
